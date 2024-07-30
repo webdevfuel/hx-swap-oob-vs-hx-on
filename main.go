16d693d9063dd66c14b57cbcc125a35131d559d6
Emanuel Turis <hello@emanuelturis.com>
2024-07-30 14:04:32 +0300

    add hx-on example with sidebar

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -13,7 +14,13 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		component := template.Hello("world")
+		component := template.Index()
+		component.Render(r.Context(), w)
+	})
+	r.Post("/items", func(w http.ResponseWriter, r *http.Request) {
+		value := r.FormValue("item")
+		w.Header().Set("Hx-Trigger", fmt.Sprintf(`{"addItem": {"value": "%s"}}`, value))
+		component := template.FormWithLatestItem(value)
 		component.Render(r.Context(), w)
 	})
 	http.ListenAndServe("localhost:3000", r)
